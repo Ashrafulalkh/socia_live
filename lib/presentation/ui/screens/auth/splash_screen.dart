aee54ec6b904fbb7341a0d94fdb907bbe0ef60f9
Md. Ashraful Alam Khan <160164446+Ashrafulalkh@users.noreply.github.com>
2024-11-28 22:32:53 +0600

    Develop (#3)
    * -login screen created
    
    * - Added splash screen (#2)
    
    - add logo
    - add theme
    
    ---------
    
    Co-authored-by: Shougot_mollik <113712457+Shougotmollik@users.noreply.github.com>

--- lib/presentation/ui/screens/auth/splash_screen.dart
+++ lib/presentation/ui/screens/auth/splash_screen.dart
@@ -1,12 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:get/get.dart';
+import 'package:socia_live/presentation/route/route_names.dart';
 
-class SplashScreen extends StatelessWidget {
+import 'package:socia_live/presentation/ui/utils/assets_path.dart';
+
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  Future<void> _moveToNextScreen() async {
+    await Future.delayed(const Duration(seconds: 2));
+    Get.offNamed(RouteNames.welcomeScreen);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _moveToNextScreen();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: SafeArea(
+          child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Spacer(),
+            SvgPicture.asset(AppAssetsPath.appLogo, height: 50),
+            const Spacer(),
+            const CircularProgressIndicator(),
+            const SizedBox(height: 16),
+            const Text(
+              'version 1.0.0',
+              style: TextStyle(color: Colors.grey),
+            ),
+          ],
+        ),
+      )),
     );
   }
 }
---
+++ lib/presentation/ui/utils/assets_path.dart
@@ -0,0 +1,4 @@
+class AppAssetsPath {
+
+  static const String appLogo="assets/logo/SociaLive.svg";
+}
