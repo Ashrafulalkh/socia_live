a3a73ee9e90adfd51880c12811280ae575716e1c
Md. Ashraful Alam Khan <160164446+Ashrafulalkh@users.noreply.github.com>
2024-11-28 23:13:46 +0600

    Develop (#5)
    * -login screen created
    
    * - Added splash screen (#2)
    
    - add logo
    - add theme
    
    * - design welcome screen (#4)
    
    - design login screen
    - design sing up screen
    - design main bottom nav bar screen
    
    Co-authored-by: Md. Ashraful Alam Khan <160164446+Ashrafulalkh@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Shougot_mollik <113712457+Shougotmollik@users.noreply.github.com>

--- lib/app.dart
+++ lib/app.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:socia_live/presentation/ui/screens/auth/splash_screen.dart';
+import 'package:socia_live/controller_binder.dart';
+import 'package:socia_live/presentation/route/route_names.dart';
+import 'package:socia_live/presentation/route/routes.dart';
+import 'package:socia_live/presentation/theme/theme.dart';
+
+import 'package:socia_live/presentation/ui/utils/app_strings.dart';
 
 class SociaLive extends StatelessWidget {
   const SociaLive({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp(
-      home: SplashScreen(),
+    return GetMaterialApp(
+      title: AppStrings.appName,
+      theme: darkTheme,
+      getPages: Routes.pages,
+      initialRoute: RouteNames.splashScreen,
+      defaultTransition: Transition.noTransition,
+      initialBinding: ControllerBinder(),
     );
   }
 }
---
+++ lib/presentation/ui/screens/auth/welcome_screen.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:get/get.dart';
+import 'package:socia_live/presentation/route/route_names.dart';
+import 'package:socia_live/presentation/ui/utils/app_assets_path.dart';
+
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+
+            SvgPicture.asset(AppAssetsPath.appLogo, height: 35),
+            const SizedBox(height: 24),
+            _buildSignUpSection(),
+            const SizedBox(height: 12),
+            _buildLonginSection()
+          ],
+        ),
+      )),
+    );
+  }
+
+  Widget _buildLonginSection() {
+    return TextButton(
+        onPressed: _onTapLoginScreen,
+        child: const Text(
+          "Log In",
+          style: TextStyle(fontWeight: FontWeight.w600, fontSize: 18),
+        ));
+  }
+
+  Widget _buildSignUpSection() {
+    return ElevatedButton(
+      onPressed: _onTapSignupScreen,
+      style: ButtonStyle(
+        minimumSize: WidgetStateProperty.all(const Size(300, 45)),
+      ),
+      child: const Text("Create Account"),
+    );
+  }
+
+  void _onTapLoginScreen() {
+    Get.toNamed(RouteNames.loginScreen);
+  }
+
+  void _onTapSignupScreen() {
+    Get.toNamed(RouteNames.signupScreen);
+  }
+}
