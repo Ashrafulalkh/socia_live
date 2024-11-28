155bf86f32d35704ade32a52ac408564aebee44e
nafiurrahmansabbir <nafiurrahmansabbir@gmail.com>
2024-11-29 02:56:39 +0600

    SM6-7: Utils: App_Colors added and theme Color changed follow by UI Design

---
+++ lib/presentation/ui/utils/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color darkThemeAppBarColor = Color(0xFF1D2939);
+  static const Color darkThemeBodyColor = Color(0xFF101828);
+
+}
