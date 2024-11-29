e72475789a9846fcc1bbb4bc63bb4d64e2eacc1a
nafiurrahmansabbir <nafiurrahmansabbir@gmail.com>
2024-11-30 00:28:38 +0600

    SM6-8: app_colors add

--- lib/presentation/ui/utils/app_colors.dart
+++ lib/presentation/ui/utils/app_colors.dart
@@ -3,5 +3,6 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const Color darkThemeAppBarColor = Color(0xFF1D2939);
   static const Color darkThemeBodyColor = Color(0xFF101828);
+  static const Color darkThemeProfileButtonColor = Color(0xFF3C5071);
 
 }
