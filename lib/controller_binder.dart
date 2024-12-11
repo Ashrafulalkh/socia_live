29bce837c023c598a89a2558a9e324e67e46bc53
Ashrafulalkh <ashrafulkhan194@gmail.com>
2024-12-11 23:22:28 +0600

    -fetch user data from database table -show fetched data in my profile screen

--- lib/controller_binder.dart
+++ lib/controller_binder.dart
@@ -4,6 +4,7 @@ import 'package:socia_live/presentation/state_holders/auth/sign_in_controller.da
 import 'package:socia_live/presentation/state_holders/auth/sign_up_controller.dart';
 import 'package:socia_live/presentation/state_holders/navigation_screen/bottom_nav_bar_controller.dart';
 import 'package:socia_live/presentation/state_holders/theme/theme_controller.dart';
+import 'package:socia_live/presentation/state_holders/user_details/user_details_controller.dart';
 
 class ControllerBinder extends Bindings {
   @override
@@ -11,6 +12,7 @@ class ControllerBinder extends Bindings {
     Get.put(AuthController());
     Get.put(SignUpController());
     Get.put(BottomNavBarController());
+    Get.put(UserDetailsController());
     Get.put(SignInController());
     Get.put(ThemeController());
 
