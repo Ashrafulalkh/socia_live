635caef53df9e6202b74f85bc035297b6fd097d2
Md. Ashraful Alam Khan <160164446+Ashrafulalkh@users.noreply.github.com>
2024-12-11 23:24:46 +0600

    Develop (#32)
    * -login screen created
    
    * - Added splash screen (#2)
    
    - add logo
    - add theme
    
    * - design welcome screen (#4)
    
    - design login screen
    - design sing up screen
    - design main bottom nav bar screen
    
    Co-authored-by: Md. Ashraful Alam Khan <160164446+Ashrafulalkh@users.noreply.github.com>
    
    * -Sign up proccess implemented (#6)
    
    -Data saved in supabase database
    -some changes also done
    
    * Feature/sm6 7/my profile screen (#9)
    
    * SM6-7: Add My Profile Screen 1st commit
    
    * SM6-7: Utils: App_Colors added and theme Color changed follow by UI Design
    
    * SM6-7: My_profile Screen Update 2nd commit
    
    * SM6-7: My_profile Screen Update 3nd commit
    
    * SM6-8: others profile screen work
    
    * SM6-8: app_colors add
    
    * SM6-7 & SM6-8: My and others profile screen UI done
    
    * edited pubspec
    
    * SM6-7 & SM6-8: conflict resolve
    
    * SM6-7 & SM6-8:  InstaImageViewer added on profile Screen
    
    * SM6-7 & SM6-8: asset path fixt
    
    * SM6-7 & SM6-8: asset path fixt
    
    * PhotoSelectionDialog (#14)
    
    * SM6-13: Photo Selection Dialog Widget added
    
    * SM6-13: Photo Selection Dialog Widget added
    
    * SM6-13:  Photo Selection Dialog Widget added
    
    * - signInAuthentication with supabase (#12)
    
    - signInController
    
    * - Implement light theme (#17)
    
    - fixed some themed bugs
    
    Co-authored-by: Nafiur Rahman Sabbir <nafiurrahmansabbir@gmail.com>
    
    * Develop: Pull conflict solve
    
    * Feature/sm6 14/profile screens responsive and reusable (#19)
    
    * SM6-14:new gridview add and reusable widget
    
    * SM6-14: Theming color bug fixed
    
    * SM6-14:
    
    * SM6-14: Profile Screen Done
    
    * - Implement light theme (#20)
    
    - fixed some themed bugs
    - Theme controlled by getx
    
    * - signInAuthentication with supabase (#18)
    
    - signInController
    
    Co-authored-by: Pranta-Nag <prantanag10@gmail.com>
    Co-authored-by: Md. Ashraful Alam Khan <160164446+Ashrafulalkh@users.noreply.github.com>
    
    * develop: small bug fix
    
    * Search Screen (#23)
    
    * SM-18: Design Search Screen first commit
    
    * SM-18: Add device previewer
    
    * SM-18: Design Search Screen 2nd commit
    
    * SM-18: Design Search Screen 3nd commit
    
    * - Design home screen app bar (#25)
    
    - post card
    
    * -add sharedpreference package (#27)
    
    -create auth controller
    -save access token during sign in
    -navigate based on the token
    
    * - design home screen's Story card (#29)
    
    * -fetch user data from database table (#31)
    
    -show fetched data in my profile screen
    
    ---------
    
    Co-authored-by: Shougot_mollik <113712457+Shougotmollik@users.noreply.github.com>
    Co-authored-by: Nafiur Rahman Sabbir <nafiurrahmansabbir@gmail.com>
    Co-authored-by: Pranta Nag <prantanag10@gmail.com>

--- lib/presentation/ui/screens/navigation_screen/my_profile_screen.dart
+++ lib/presentation/ui/screens/navigation_screen/my_profile_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:socia_live/presentation/state_holders/navigation_screen/bottom_nav_bar_controller.dart';
+import 'package:socia_live/presentation/state_holders/user_details/user_details_controller.dart';
+import 'package:socia_live/presentation/ui/screens/navigation_screen/others_profile_screen.dart';
 import 'package:socia_live/presentation/ui/widgets/profile_widgets/profile_steaming_list.dart';
 import '../../utils/app_assets_path.dart';
 import '../../widgets/profile_widgets/my_profile_info.dart';
@@ -14,6 +15,13 @@ class MyProfileScreen extends StatefulWidget {
 }
 
 class _MyProfileScreenState extends State<MyProfileScreen> {
+
+  @override
+  void initState() {
+    Get.find<UserDetailsController>().fetchUserDetails();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
@@ -34,30 +42,40 @@ class _MyProfileScreenState extends State<MyProfileScreen> {
             ),
           ),
         ),
-        body: Column(
-          children: [
-            MyProfileInfoWidget(
-              followingCallback: () {},
-              followersCallback: () {},
-            ),
-            _buildTabBar(),
-            const SizedBox(
-              height: 10,
-            ),
-            Expanded(
-              child: TabBarView(
+        body: GetBuilder<UserDetailsController>(
+          builder: (userDetailsController) {
+            return Visibility(
+              visible: !userDetailsController.inProgress,
+              replacement: const Center(
+                child: CircularProgressIndicator(),
+              ),
+              child: Column(
                 children: [
-                  ProfilePhotoGalleryListGridView(
-                    imageList: AppAssetsPath().demoImageList,
+                  MyProfileInfoWidget(
+                    followingCallback: () {},
+                    followersCallback: () {},  user: userDetailsController.user,
                   ),
-                  const ProfileSteamingList(),
-                  ProfilePhotoGalleryListGridView(
-                    imageList: AppAssetsPath().demoImageList,
+                  _buildTabBar(),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Expanded(
+                    child: TabBarView(
+                      children: [
+                        ProfilePhotoGalleryListGridView(
+                          imageList: AppAssetsPath().demoImageList,
+                        ),
+                        const ProfileSteamingList(),
+                        ProfilePhotoGalleryListGridView(
+                          imageList: AppAssetsPath().demoImageList,
+                        ),
+                      ],
+                    ),
                   ),
                 ],
               ),
-            ),
-          ],
+            );
+          }
         ),
       ),
     );
